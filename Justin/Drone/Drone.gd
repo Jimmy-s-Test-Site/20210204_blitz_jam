46693931f9f65eb51511038168bb608684de3d9c
Justin <justp02@hotmail.com>
2021-02-05 21:29:25 -0600

    Fixed Drone crashing
    With more than 1 drone the game crashes.  Array set-up for paths, just need to 'call it' for movement!

--- Justin/Drone/Drone.gd
+++ Justin/Drone/Drone.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
 #export (Array, PackedScene) var rooms : Array
-export (int) var speed := 500 #drone movement speed
-export (float) var bulletDelay := 0.7 #delay before next bullet fires
-export (PackedScene) var projectile #droppable container for projectile scene
+export (Array, Vector2) var DronePath# := Vector2()	#The # and location of each Stop on Path
+#export (Array,Transform) var dronePath[NumberDronePaths] : Vector2
+export (int) var Speed := 500 						#drone movement speed
+export (float) var BulletDelay := 0.7 				#delay before next bullet fires
+export (PackedScene) var Projectile 				#droppable container for projectile scene
 export (NodePath) var ProjectileContainer
-onready var shoot_Timer = $bullet_Timer #A timer node called bullet_Timer
-export (int) var energy := 3 #drone energy level
-export (Vector2) var shootingAt := Vector2(-1,0)  #what it's target is. Player/Mannequin/free shooting
+export (int) var Energy := 3 						#drone energy level
+export (Vector2) var ShootingAt := Vector2(-1,0)    #what it's target is. Player/Mannequin/free shooting
 
+onready var shoot_Timer = $bullet_Timer #A timer node called bullet_Timer
 
 func _ready():
-	shoot_Timer.start(bulletDelay) #start bullet timer
+	shoot_Timer.start(BulletDelay) #start bullet timer
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,13 +21,13 @@ func _process(delta):
 	pass
 	
 func shoot():
-	var bullet_directions = shootingAt
-	var new_bullet = projectile.instance()
-	new_bullet.global_position = self.global_position + $SpawnRadius.position.length() * shootingAt
+	var bullet_directions = ShootingAt
+	var new_bullet = Projectile.instance()
+	new_bullet.global_position = self.global_position + $SpawnRadius.position.length() * ShootingAt
 	new_bullet.name = "Projectile"
-	new_bullet.direction = self.shootingAt
-	get_node(ProjectileContainer).add_child(new_bullet)
-	
+	new_bullet.direction = self.ShootingAt
+	#get_node(ProjectileContainer).add_child(new_bullet)
+	$ProjectileContainer.add_child(new_bullet)
 
 func _on_Drone_tree_entered(): #if drone is touched then damage player
 	print ("Drone Touched")
@@ -33,4 +35,4 @@ func _on_Drone_tree_entered(): #if drone is touched then damage player
 
 func _on_bullet_Timer_timeout():#on timeout shoot and restart timer
 	shoot()
-	shoot_Timer.start(bulletDelay)
+	shoot_Timer.start(BulletDelay)
