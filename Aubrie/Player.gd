845f18552b163b25037cec39960d155aa356be99
Aubrie <aubriemerrell4452@gmail.com>
2021-02-05 19:54:10 -0600

    Character movement

--- Aubrie/Player.gd
+++ Aubrie/Player.gd
@@ -1,32 +1,36 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+const speed =100
+const gravity = 10
+const jump = -200
+const Floor = Vector2(0, -1)
 
-var target = Vector2()
 var velocity = Vector2()
-# Declare member variables here, movement actions
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('left'):
-		velocity.x += 1
-	if Input.is_action_pressed('right'):
-		velocity.x -= 1
-#	if Input.is_action_pressed('jump'):
-#		velocity.x += 1
-	
-	velocity = velocity.normalized() * speed
-	
-func _physics_process(delta):
-	get_input()
-	velocity = move_and_slide(velocity)
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var on_ground = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("left"): 
+		velocity.x = -speed 
+	elif Input.is_action_pressed("right"):
+		velocity.x = speed
+	else:
+		velocity.x =0 
+	
+	if Input.is_action_pressed("jump"):
+		if on_ground == true:
+			velocity.y = jump
+			on_ground = false
+#	if is_on_floor():
+#		on_ground = true
+#	else: 
+#		on_ground = false
+	on_ground = is_on_floor()
+		
+	velocity.y += gravity
+	velocity = move_and_slide(velocity, Floor)
+	
+	
+
+
+
