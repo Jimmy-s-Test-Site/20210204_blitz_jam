3d5600250234b0c7521bc5f6ae76fc8ae461719c
jaacko-torus <jaacko.torus@gmail.com>
2021-02-06 14:06:49 -0500

    finished room translated

--- Julian/Rooms/Room.gd
+++ Julian/Rooms/Room.gd
@@ -5,21 +5,16 @@ signal goto_room
 
 const room := preload("res://Julian/Rooms/Rooms.gd")
 
-export (Array, NodePath) var exits
+export (Array, NodePath) var door_paths
 
 func _ready() -> void:
-	for exit in self.exits:
-		var exit_scene : Area2D = self.get_node(exit)
-		
-		self.on_Door_body_entered(exit_scene)
-		#if not exit_scene.is_connected("body_entered", self, "on_Door_body_entered"):
-		#	exit_scene.connect("body_entered", self, "on_Door_body_entered")
+	for door_path in self.door_paths:
+		var door : Area2D = self.get_node(door_path)
+		self.on_Door_body_entered(door)
 
-func on_Door_body_entered(exit_scene : Area2D) -> void:
-	yield(exit_scene, "body_entered")
-	self.exit(exit_scene.destination)
+func on_Door_body_entered(door : Area2D) -> void:
+	yield(door, "body_entered")
+	self.exit(door)
 
-func exit(exit_name : int) -> void:
-	self.emit_signal("goto_room", exit_name)
-
-
+func exit(door : Area2D) -> void:
+	self.emit_signal("goto_room", door.destination)
