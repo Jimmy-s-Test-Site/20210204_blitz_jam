53d8edf06c954f6bb141e24a9153a6ef994c0941
jaacko-torus <jaacko.torus@gmail.com>
2021-02-07 00:36:03 -0500

    added all 5 rooms plus 1 end room

--- Julian/Rooms/Rooms.gd
+++ Julian/Rooms/Rooms.gd
@@ -2,10 +2,20 @@ extends Node
 
 enum Names {
 	Room0,
-	Room1
+	Room1,
+	Room2,
+	Room3,
+	Room4,
+	
+	RoomEnd,
 }
 
 const Scenes = {
 	Names.Room0: "res://Julian/Rooms/Room0/Room0.tscn",
-	Names.Room1: "res://Julian/Rooms/Room1/Room1.tscn"
+	Names.Room1: "res://Julian/Rooms/Room1/Room1.tscn",
+	Names.Room2: "res://Julian/Rooms/Room2/Room2.tscn",
+	Names.Room3: "res://Julian/Rooms/Room3/Room3.tscn",
+	Names.Room4: "res://Julian/Rooms/Room4/Room4.tscn",
+	
+	Names.RoomEnd: "res://Julian/Rooms/RoomEnd/RoomEnd.tscn"
 }
