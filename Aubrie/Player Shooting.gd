ff76238fc233c4f444355e2fdd1d2aa553639e2f
Aubrie <aubriemerrell4452@gmail.com>
2021-02-06 18:40:50 -0600

    Adding shooting for player

---
+++ Aubrie/Player Shooting.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+const Speed = 100 
+var velocity = Vector2()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	velocity.x = Speed * delta
+	translate(velocity)
+		
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
