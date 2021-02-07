f675555e64c3a1568cfa322a2994e73139a1f2f0
Aubrie <aubriemerrell4452@gmail.com>
2021-02-06 23:57:20 -0600

    Changed Position2D

--- Aubrie/Player Shooting.gd
+++ Aubrie/Player Shooting.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 const Speed = 100 
 var velocity = Vector2()
+var direction = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,13 +12,10 @@ func _ready():
 func _physics_process(delta):
 #	velocity.x = Speed * delta
 #	translate(velocity)
-	position += velocity * Speed *delta
+	position += velocity * Speed * delta * direction
 	$AnimatedSprite.play("shoot")
 		
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
-func _on_Player_Shooting_body_entered(body):
-	if body.name == "Player":
-		pass
-	
+
