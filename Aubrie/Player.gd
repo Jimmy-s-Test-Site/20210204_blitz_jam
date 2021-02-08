98207590e7f7368612860c13cf0fadcb34025cd0
Aubrie <aubriemerrell4452@gmail.com>
2021-02-07 23:35:50 -0600

    Player Animation
    added sprites for animation, player

--- Aubrie/Player.gd
+++ Aubrie/Player.gd
@@ -16,6 +16,7 @@ export (NodePath) var BulletContainerPath
 
 var velocity = Vector2()
 var on_ground = false
+var is_attacking = false
 #var BagPickup = null
 #var BagDrop = null
 
@@ -27,28 +28,33 @@ var on_ground = false
 func _physics_process(delta):
 	if Input.is_action_pressed("right"): 
 		velocity.x = speed
-		$AnimatedSprite.play("run")
+		$AnimatedSprite.play("Run")
+		$AnimatedSprite2.play("Run")
 #		$AnimatedSprite.flip_h = false
 #		if sign($Position2D.position.x) == -1:
 #			$Position2D.position.x *= -1
 	elif Input.is_action_pressed("left"):
 		velocity.x = -speed
-		$AnimatedSprite.play("run")
+		$AnimatedSprite.play("Run")
+		$AnimatedSprite2.play("Run")
 #		$AnimatedSprite.flip_h = true
 #		if sign($Position2D.position.x) == 1:
 #			$Position2D.position.x *= -1
 	else:
 		velocity.x =0 
 		if on_ground == true:
-			$AnimatedSprite.play("Idle")
+			$AnimatedSprite.play("Idle")	
+			$AnimatedSprite2.play("Idle")
 			
 	match int(sign(get_local_mouse_position().x)):
 		-1:
 			$AnimatedSprite.flip_h = true
+			$AnimatedSprite2.flip_h = true
 			if sign($Position2D.position.x) == 1:
 				$Position2D.position.x *= -1
 		1:
 			$AnimatedSprite.flip_h = false
+			$AnimatedSprite2.flip_h = false
 			if sign($Position2D.position.x) == -1:
 				$Position2D.position.x *= -1
 				
@@ -71,6 +77,7 @@ func _physics_process(delta):
 		get_node(BulletContainerPath).add_child(bullet)
 		bullet.position = $Position2D.global_position
 		$AnimatedSprite.play("Shoot")
+		$AnimatedSprite2.play("Shoot")
 	
 	#Added 
 #	if Input.is_action_pressed ("pickup"):
@@ -101,10 +108,11 @@ func _physics_process(delta):
 	else:
 		on_ground = false
 		if velocity.y < 0:
-			$AnimatedSprite.play("jump")
+			$AnimatedSprite.play("Jump")
+			$AnimatedSprite2.play("Jump")
 		else:
 			$AnimatedSprite.play("Fall")
-			
+			$AnimatedSprite2.play("Fall")
 	velocity = move_and_slide(velocity, Floor)
 	
 
