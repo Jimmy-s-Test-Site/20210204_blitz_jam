dc3db71c8541b9e713316f6d07ba8c5d3c13431a
jaacko-torus <jaacko.torus@gmail.com>
2021-02-08 00:34:09 -0500

    QoL improvements

--- Julian/Items/Block/Block.gd
+++ Julian/Items/Block/Block.gd
@@ -3,12 +3,12 @@ extends RigidBody2D
 var is_on_floor = false
 var floor_y
 
-func _integrate_forces(state):
+func _integrate_forces(_state):
 	self.rotation = 0
 	if self.is_on_floor:
 		self.position.y = self.floor_y
 
-func _on_Block_body_entered(body):
+func _on_Block_body_entered(_body):
 	if not self.is_on_floor:
 		self.is_on_floor = true
 		self.floor_y = self.position.y
