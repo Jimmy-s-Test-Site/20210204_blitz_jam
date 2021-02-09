7edcd222bd82d41944aba07c645b917fb461d9ea
jaacko-torus <jaacko.torus@gmail.com>
2021-02-09 16:50:35 -0500

    added mannequins

--- Julian/Rooms/Room.gd
+++ Julian/Rooms/Room.gd
@@ -15,16 +15,26 @@ enum ENDING_SCENES {
 }
 
 export(NodePath) var door_container_path
+export(NodePath) var mannequin_container_path
 
 func _ready() -> void:
 	var door_container : Node2D = self.get_node(self.door_container_path)
+	var mannequin_container : Node2D = self.get_node(self.mannequin_container_path)
 	
 	for door in door_container.get_children():
 		self.on_Door_body_entered(door)
+	
+	for mannequin in mannequin_container.get_children():
+		self.on_Mannequin_added_points(mannequin)
 
 func on_Door_body_entered(door : Area2D) -> void:
 	yield(door, "body_entered")
 	self.exit(door)
+
+func on_Mannequin_added_points(mannequin : Sprite):
+	yield(mannequin, "add_points")
+	
+	self.emit_signal("add_points", mannequin.points)
 
 func exit(door : Area2D) -> void:
 	self.emit_signal("add_points", door.points)
--- Justin/Mannequin/Mannequin.gd
+++ Justin/Mannequin/Mannequin.gd
@@ -1,24 +1,19 @@
 extends Sprite
 
+signal add_points
 
-export (int) var Health :=3 
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export(int) var Health := 3 
+export(int) var points := 1
 
 func _on_MannequinArea_body_entered(body):
 	var bodyIsPlayer = body.name == "Player"						#if Player enters MannequinArea then bodyIsPlayer is true
 	var bodyIsDrone = body.name.begins_with("Drone")				#if Drone enters MannequinArea then bodyIsDrone is true
 	var bodyIsProjectile = body.name.begins_with("Projectile")		#if Projectile enters MannequinArea then bodyIsProjectile is true
 	if bodyIsPlayer:	#if bodyIsPlayer is true -> get 5 points and make mannequin disappear
-		#points+5
+		self.emit_signal("add_points", self.points)
 		queue_free() 
 	elif bodyIsDrone or bodyIsProjectile:	#if bodyIsDrone/Projetile is true -> make mannequin disappear
 		queue_free()
-		
 
 func _on_MannequinArea_area_entered(area):
 	var areaIsProjectile = area.name.begins_with("PlayerShooting")		#if Projectile enters MannequinArea then bodyIsProjectile is true
