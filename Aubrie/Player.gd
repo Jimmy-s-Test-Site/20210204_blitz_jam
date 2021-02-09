e192b28b1eb7609a8a67aedc8036dc0135d1d46c
jaacko-torus <jaacko.torus@gmail.com>
2021-02-09 15:43:02 -0500

    Auto stash before merge of "main" and "origin/main"

--- Aubrie/Player.gd
+++ Aubrie/Player.gd
@@ -1,45 +1,31 @@
 extends KinematicBody2D
 
-#const speed =175
-#const gravity = 10
-#const jump = -300
 const Floor = Vector2(0, -1)
 const Shoot = preload("res://Aubrie/Player Shooting.tscn")
-#Added for pickup
-#const PickUp = preload ("res://Aubrie/Bag.tscn")
 
 export (int) var speed = 200
 export (int) var gravity = 10
 export (int) var jump = -300
 export (NodePath) var BulletContainerPath
-#export (NodePath) var BagPickDrop
 
 var velocity = Vector2()
 var on_ground = false
 var is_attacking = false
 var can_shoot = false
-#var BagPickup = null
-#var BagDrop = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		self.can_shoot = true
+	
 	if Input.is_action_pressed("right"): 
 		velocity.x = speed
 		$AnimatedSprite.play("Run")
 		$AnimatedSprite2.play("Run")
-#		$AnimatedSprite.flip_h = false
-#		if sign($Position2D.position.x) == -1:
-#			$Position2D.position.x *= -1
 	elif Input.is_action_pressed("left"):
 		velocity.x = -speed
 		$AnimatedSprite.play("Run")
 		$AnimatedSprite2.play("Run")
-#		$AnimatedSprite.flip_h = true
-#		if sign($Position2D.position.x) == 1:
-#			$Position2D.position.x *= -1
-	
 	elif self.can_shoot == true:
 		if Input.is_action_just_pressed("shoot"):
 			var bullet = Shoot.instance()
@@ -55,19 +41,21 @@ func _physics_process(delta):
 			bullet.position = $Position2D.global_position
 		$AnimatedSprite.play("Shoot")
 		$AnimatedSprite2.play("Shoot")
-	
-	#push/pull
-#	elif Input.is_action_just_pressed("pull"):
-#		if is_on_wall():
-#
-#		collision.collider.add_force(collision.position, Vector2(speed * 20,0))
-
+	elif Input.is_action_just_pressed("pull"):
+		for body in $PushPullArea.get_overlapping_bodies():
+			print(body.name)
+		pass
+		# push/pull
+		# for i in get_slide_count():
+		#	var collision = get_slide_collision(i)
+		#	if collision.collider.name.begins_with("Block"):
+		#		collision.collider.add_central_force(Vector2(speed * 20, 0))
 	else:
 		velocity.x =0 
 		if on_ground == true:
-			$AnimatedSprite.play("Idle")	
+			$AnimatedSprite.play("Idle")
 			$AnimatedSprite2.play("Idle")
-			
+	
 	match int(sign(get_local_mouse_position().x)):
 		-1:
 			$AnimatedSprite.flip_h = true
@@ -106,10 +94,8 @@ func _physics_process(delta):
 #				if collision.collider.name.begins_with("Bag"):
 #					self.BagDrop = collision.collider
 #			self.BagDrop
-#
-		
+	
 	velocity.y += gravity
-	#on_ground = is_on_floor()
 	if is_on_floor():
 		on_ground = true
 	else:
@@ -121,8 +107,11 @@ func _physics_process(delta):
 			$AnimatedSprite.play("Fall")
 			$AnimatedSprite2.play("Fall")
 	velocity = move_and_slide(velocity, Floor)
-	
+
 func _on_AnimatedSprite2_animation_finished():
-	pass # Replace with function body.
 	if $AnimatedSprite2.animation == "Shoot":
 		self.can_shoot = false
+
+func _on_PushPullArea_body_entered(body):
+	if body.name.begins_with("Block"):
+		self.can_push = true
